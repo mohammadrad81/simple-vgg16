6229910d9bf1e26205f4e12f72db50e510721541
mohammadrad81 <mohammad.rad81@gmail.com>
2024-06-27 10:57:32 +0330

    syntax errors

--- phase_2.cu
+++ phase_2.cu
@@ -8,8 +8,8 @@
 #define POOL_ERROR "error in pooling!"
 #define CNN_ERROR "error in CNN!"
 #define FC_ERROR "error in FC!"
-#define CEIL_DIV(int a, int b) ((n - 1) / b + 1)
-#define MIN(float a, float b) (a < b? a : b)
+#define CEIL_DIV(a, b) ((a + b - 1) / b)
+#define MIN(a, b) (a < b? a : b)
 
 void handle(cudaError_t status, char* message){
     if(status != cudaSuccess){
@@ -26,10 +26,8 @@ __global__ void pad_kernel(float* dev_input, float* dev_output, int width, int h
     int position = (channel * width * height) + (row * width) + col;
     float value = 0.0;
     if((row < height + 2 * pad_h) && (col < width + 2 * pad_w) && (channel < channels)){
-        if(){ // in pad
-            if((row < pad_h) || (row >= height + pad_h) || (col < pad_w) || (col >= width + pad_w)){
+        if((row < pad_h) || (row >= height + pad_h) || (col < pad_w) || (col >= width + pad_w)){ // in pad
                 value = 0;
-            }
         }
         else{ // not in pad
             value = dev_output[position - ((pad_h * width) + pad_w)];
@@ -39,21 +37,21 @@ __global__ void pad_kernel(float* dev_input, float* dev_output, int width, int h
 }
 
 float* pad_with_cuda(float* dev_input_image, int width, int height, int channels, int pad_w, int pad_h){
-    int input_image_size = width * heigth * channels;
+    int input_image_size = width * height * channels;
     int output_image_width = (width + 2 * pad_w);
     int output_image_height = (height + 2 * pad_h);
     int output_image_size = output_image_width * output_image_height * channels * sizeof(float);
     float* dev_output_image = 0;
-    handle(cudaMalloc((void**)&dev_output_image, output_image_size, MALLOC_ERROR));
-    gridDim(CEIL_DIV(width, 32), CEIL_DIV(height), channels);
-    blockDim(32, 32, 1);
+    handle(cudaMalloc((void**)&dev_output_image, output_image_size), MALLOC_ERROR);
+    dim3 gridDim(CEIL_DIV(width, 32), CEIL_DIV(height, 32), channels);
+    dim3 blockDim(32, 32, 1);
     pad_kernel<<<gridDim, blockDim>>>(dev_input_image, dev_output_image, width, height, channels, pad_w, pad_h);
     handle(cudaDeviceSynchronize());
     return dev_output_image;
 }
 
 void max_pool_2D_kernel(float* dev_input_image, float* dev_output_image, int width, int height, int channels, int output_width, int output_height){
-    __shared__ part[64][16];
+    __shared__ float part[64][16];
     int row_in_block = threadIdx.y;
     int col_in_block = threadIdx.x;
     int block_row = blockIdx.y;
@@ -63,7 +61,7 @@ void max_pool_2D_kernel(float* dev_input_image, float* dev_output_image, int wid
     int channel = blockIdx.z;
     int load_position = (channel * width * height) + (row * width) + col;
     int in_input_image = row < height && col < width;
-    float value = 0.0
+    float value = 0.0;
     if(in_input_image){
         value = dev_input_image[load_position];
     }
@@ -80,9 +78,9 @@ void max_pool_2D_kernel(float* dev_input_image, float* dev_output_image, int wid
         int store_position = (channel * output_width * output_height) + (store_row * output_width) + store_col;
         int row_in_part = 2 * row;
         int col_in_part = 2 * col;
-        value = MIN(part[row_in_part][col_in_part], row_in_part[row_in_part][col_in_part + 1])
-        value = MIN(value, part[row_in_part + 1][col_in_part])
-        value = MIN(value, part[row_in_part + 1][col_in_part + 1])
+        value = MIN(part[row_in_part][col_in_part], part[row_in_part][col_in_part + 1]);
+        value = MIN(value, part[row_in_part + 1][col_in_part]);
+        value = MIN(value, part[row_in_part + 1][col_in_part + 1]); 
         dev_output_image[store_position] = value;
     }
 }
@@ -92,12 +90,16 @@ float* max_pool_2D_with_cuda(float* dev_input_image, int width, int height, int 
     int output_image_width = width / 2;
     int output_image_height = height / 2;
     int output_image_size = output_image_width * output_image_height * channels * sizeof(float);
-    handle(cudaMalloc((void**)&dev_output_image, output_image_size, MALLOC_ERROR));
-    gridDim(CEIL_DIV(width, 64), CEIL_DIV(height, 16), channels);
-    blockDim(64, 16, 1);
-    max_pool_2D_kernel<<<gridDim, blockDim>>>(dev_input_image, dev_output_image, width, height, channels, output_image_width, output_height);
-    handle(cudaDeviceSynchronize());
+    handle(cudaMalloc((void**)&dev_output_image, output_image_size), MALLOC_ERROR);
+    dim3 gridDim(CEIL_DIV(width, 64), CEIL_DIV(height, 16), channels);
+    dim3 blockDim(64, 16, 1);
+    max_pool_2D_kernel<<<gridDim, blockDim>>>(dev_input_image, dev_output_image, width, height, channels, output_image_width, output_image_height);
+    handle(cudaDeviceSynchronize(), POOL_ERROR);
     return dev_output_image;
+}
+
+float* conv2D_with_cuda(float* dev_input_image, int width, int height, int channels, float* kernel, int k_width, int k_height){
+
 }
 
 float rand_float(){
@@ -113,9 +115,8 @@ void fill_array(float* array, int length){
 
 float* vgg_16(float* host_images, int channels, int width, int height){
     float* dev_images = 0;
-    int image_vector_length = channels * width * height * sizeof(float);
-    handle(cudaMalloc((void**)&dev_images, image_vector_length, MALLOC_ERROR));
-    handle(cudaMemcpy(dev_images, host_images, image_vector_length));
+    int image_vector_size = channels * width * height * sizeof(float);
+    handle(cudaMalloc((void**)&dev_images, image_vector_size), MALLOC_ERROR);
 
 
 }
